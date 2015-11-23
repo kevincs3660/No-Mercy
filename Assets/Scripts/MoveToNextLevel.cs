38e9157caeb4d3d1a28355ab34b45882b1beb0f4
Kevin Faustino <u0822559@utah.edu>
2015-11-23 15:42:34 -0700

    Movetonextlevel

---
+++ Assets/Scripts/MoveToNextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToNextLevel : MonoBehaviour {
+
+	public string nextLevel;
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		PlayerController2 player = collider.gameObject.GetComponent<PlayerController2>();
+		if(player != null)
+			Application.LoadLevel(nextLevel);
+	}
+
+}
